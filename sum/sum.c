b44deaeacd424f381abc365cfd28be16dd723787
semsol23 <semsol23@naver.com>
2020-09-10 18:11:40 +0900

    sum

---
+++ sum/sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	int b;
+	int sum;
+	
+	a = 10;
+	b = 20;
+	sum = a+b;
+	
+	printf("%d + %d = %d\n", a, b, a+b);
+	return 0;
+}
